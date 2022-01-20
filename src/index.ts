0111eea479adce2a00ab81924dd3702f99f1908b
Izaak Chater <izaak.chater@rea-group.com>
2022-01-20 15:08:51 +1100

    fixed unable to find postgres db error, added dependencies for express and graphql

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import {MikroORM} from "@mikro-orm/core";
-import { Post } from "./entities/Post";
+// import { dirname } from "path";
+// import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 
 
 
 const main = async () => {
+   console.log("dirname:" + __dirname)
+   
    const orm = await MikroORM.init(mikroConfig);
-   const post = orm.em.create(Post, {title: 'my first post' })
-   await orm.em.persistAndFlush(post)
-   console.log('--------- sql 2 -----------')
-   await orm.em.nativeInsert(Post, {title: 'my first post 2' })
+   await orm.getMigrator().up()
+   // const post = orm.em.create(Post, {title: 'my first post' })
+   // await orm.em.persistAndFlush(post)
+   // console.log(post)
+   // const posts = await orm.em.find(Post, {title: 'my first post' });
+   // console.log(posts)
+
 }
 
 main().catch(err => { console.error(err)})
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core"
+import path from "path";
+
+
+
+export default {
+    migrations: {
+        path: path.join(__dirname, '/migrations'), // path to folder with migration files
+        // pattern: /^[\w-]+\d+\.[tj]$/, // how to match migration files
+    },
+    entities: [Post],
+    dbName: 'lireddit',
+    type: 'postgresql',
+    user: "izaak.chater",
+    debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0];
+
