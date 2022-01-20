6bd1dc732ab14e2f242ed4d8b14885e819f9df90
Izaak Chater <izaak.chater@rea-group.com>
2022-01-20 15:17:31 +1100

    set up basics for express servers

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,21 @@ import {MikroORM} from "@mikro-orm/core";
 // import { dirname } from "path";
 // import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from 'express'
 
 
 
 const main = async () => {
-   console.log("dirname:" + __dirname)
-   
    const orm = await MikroORM.init(mikroConfig);
    await orm.getMigrator().up()
-   // const post = orm.em.create(Post, {title: 'my first post' })
-   // await orm.em.persistAndFlush(post)
-   // console.log(post)
-   // const posts = await orm.em.find(Post, {title: 'my first post' });
-   // console.log(posts)
 
+   const app = express();
+   app.get('/', (_, res)=>{
+      res.send("Hola!")
+   })
+   app.listen(4000, ()=> {
+      console.log('listening on localhost:4000')
+   })
 }
 
 main().catch(err => { console.error(err)})
