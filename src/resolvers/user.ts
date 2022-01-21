0132f21e205eac20a4c187837e4fd1cc4a3b4dee
Izaak Chater <izaak.chater@rea-group.com>
2022-01-21 15:27:05 +1100

    added in user login verification and error messages

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,5 +1,13 @@
 import { User } from "../entities/User";
-import { Mutation, Resolver, InputType, Field, Arg, Ctx } from "type-graphql";
+import {
+  Mutation,
+  Resolver,
+  InputType,
+  Field,
+  Arg,
+  Ctx,
+  ObjectType,
+} from "type-graphql";
 import { MyContext } from "../types";
 import argon2 from "argon2";
 
@@ -9,6 +17,22 @@ class UsernamePasswordInput {
   username: string;
   @Field()
   password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResonse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 
 @Resolver()
@@ -25,5 +49,27 @@ export class UserResolver {
     });
     await em.persistAndFlush(user);
     return user;
+  }
+
+  @Mutation(() => UserResonse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResonse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [{ field: "username", message: "Username doesent exist" }],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [{ field: "username", message: "password incorrect" }],
+      };
+    }
+
+    return { user };
   }
 }
