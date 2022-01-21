2b3fffc5a928924f00a5e1303bbed4edb8cfc307
Izaak Chater <izaak.chater@rea-group.com>
2022-01-21 15:09:59 +1100

    added logic to add users

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,19 +1,17 @@
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
-import { MikroORM } from "@mikro-orm/core"
+import { MikroORM } from "@mikro-orm/core";
 import path from "path";
-
-
+import { User } from "./entities/User";
 
 export default {
-    migrations: {
-        path: path.join(__dirname, '/migrations'), // path to folder with migration files
-        // pattern: /^[\w-]+\d+\.[tj]$/, // how to match migration files
-    },
-    entities: [Post],
-    dbName: 'lireddit',
-    type: 'postgresql',
-    user: "izaak.chater",
-    debug: !__prod__
+  migrations: {
+    path: path.join(__dirname, "/migrations"), // path to folder with migration files
+    // pattern: /^[\w-]+\d+\.[tj]$/, // how to match migration files
+  },
+  entities: [Post, User],
+  dbName: "lireddit",
+  type: "postgresql",
+  user: "izaak.chater",
+  debug: !__prod__,
 } as Parameters<typeof MikroORM.init>[0];
-
